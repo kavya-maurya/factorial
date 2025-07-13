bfa7d7bc936833c3e070b6b42e799d3c0aa2805b
kavya <kavyamaurya269@gmail.com>
2025-07-13 10:50:05 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+
+            int N=Convert.ToInt32(Console.ReadLine());
+            int result= 1;
+            for (int g= 1; g<=N ;g++)
+            {
+             result = result*g;
+        
+               
+            }
+                Console.WriteLine(result);
+
+
+
+        }
+      
+
+    }
+}
